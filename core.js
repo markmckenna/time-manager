80c897e26bad936763eb2edd72f3d84ca29e6c16
Mark McKenna <mark.denis.mckenna@gmail.com>
2015-05-09 01:21:03 -0300

    Decent progress--can now fetch one email

--- core.js
+++ core.js
@@ -1,31 +1,49 @@
-var clientId = 'CLIENT_ID'; // TODO: change to real client identifier
-var scopes = 'https://www.googleapis.com/auth/urlshortener'; // TODO: change this to Gmail API
+var apiKey = 'AIzaSyBIQzcxjrs9f-jv4iMB8-pGZatxqgRrcz4';
 
-function gapiAuth(authNow : boolean) {
-	gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: authNow}, function(result) {
-		var btn = document.getElementById('authorize-button');
+var gapiAuthInfo = {
+    client_id: '1020312901975-rqme2cmcjidsv6cq93o7fo8npqm617v7.apps.googleusercontent.com',
+    scope: 'https://www.googleapis.com/auth/gmail.modify',
+    immediate: false
+}
 
-		if (result && !result.error) {
-			btn.style.visibility = 'hidden';
-			gapiInitialized();
-		} else {
-			btn.style.visibility = '';
-			btn.onclick = gapiAuth;
-		}
-	});
+function gapiAuth() {
+    console.log('Authorizing...');
+	gapi.auth.authorize(gapiAuthInfo,
+        function(result) {
+            console.log('Authorized, moving on');
+            var btn = document.getElementById('authorize-button');
+
+            if (result && !result.error) {
+                btn.style.visibility = 'hidden';
+                gapiInitialized();
+            } else {
+                console.log('auth failed');
+                btn.style.visibility = '';
+                btn.onclick = gapiAuth;
+            }
+        });
 }
 
 function gapiLoaded() {
-	gapi.client.setApiKey('API_KEY'); // TODO: Add API key here
+    console.log('gapiLoaded');
+	gapi.client.setApiKey(apiKey); // TODO: Add API key here
 }
 
 function gapiInitialized() {
-		// TODO: Change to Gmail API
-		gapi.client.load('urlshortener', 'v1').then(function() {
-			document.alert('loaded!');
-		});
+    console.log('gapiInitialized');
+
+    // TODO: Change to Gmail API
+    gapi.client.load('gmail', 'v1').then(function() {
+        console.log('fetching threads');
+        var request = gapi.client.gmail.users.threads.list({'userId': 'me'});
+        request.execute(function(response) {
+            console.log('fetched about ' + response.resultSizeEstimate + ' threads');
+            var threads = response.threads;
+            for (var i = 0; i < threads.length; i++) {
+                $("#mailbox").add("li").text(threads[i].snippet)
+            };
+        });
+    });
 }
 
-$(document).ready(function() {
-	console.log('ready!');
-});
+$("#authorize-button").click(gapiAuth);
